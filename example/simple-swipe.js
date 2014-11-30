67fec5b9a053a96c3d24c21e13cdf44c86ab08dc
Marc Farra <marcfarra@gmail.com>
2014-11-30 00:34:07 -0500

    Change variable name

--- example/simple-swipe.js
+++ example/simple-swipe.js
@@ -1,12 +1,12 @@
-var Swiper = require('../')
+var swiper = require('../')
 var domready = require("domready");
 
 domready(function() {
-	new Swiper('map', {
+	var srinagar = swiper('map', {
 		original: 'devseed.SrinagarAfterFC', 
 		overlay: 'devseed.SrinagarFCBefore',
 		centroid: [34.0411824,74.8130321],
 		zoom: 11,
 		zoomControl: false
-	})	
+	});
 })	
