769479b39f4dc5b3e600187f7feb8673068b007d
Marc Farra <marcfarra@gmail.com>
2014-12-01 00:00:25 -0500

    Add info modal

--- example/querystring/querySwiper.js
+++ example/querystring/querySwiper.js
@@ -1,8 +1,16 @@
 var swiper = require('../../')
 var domready = require("domready");
 var qs = require('qs');
+var modal = require('hut-modal');
 
 domready(function() {
+	if (window.location.href.indexOf('?') == -1) {
+		window.location.href = window.location.href +
+		'?left=examples.map-i87786ca' + 
+		'&right=examples.map-i875mjb7' + 
+		'&centroid=49.434,-123.272' + 
+		'zoom=7'
+	}
 	var params = qs.parse(window.location.href.slice(window.location.href.indexOf('?') + 1));
 	if (params.centroid) {
 		params.centroid = params.centroid.split(',');
@@ -13,4 +21,9 @@ domready(function() {
 	}
 
 	var querySwiper = swiper('#map', params);
+
+	var infoModal = modal(document.querySelector('#modal'));
+	document.querySelector('#info').onclick = function() {
+		infoModal.show();
+	}
 })	
